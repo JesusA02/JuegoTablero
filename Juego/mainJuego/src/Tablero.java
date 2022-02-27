61a50c2046f1a874d8714050c0bef731e36c89e7
JesusA02 <cortesj02@estudiantec.cr>
2022-02-26 20:59:54 -0600

    add Update

--- Juego/mainJuego/src/Tablero.java
+++ Juego/mainJuego/src/Tablero.java
@@ -2,11 +2,12 @@ import javax.swing.*;
 public class Tablero{
     public int casillas = 0;
     public int numFichas = 0;
+    public int[] fichas = new int[numFichas];
 
     public Tablero(int numCasillas,int numF ){
         casillas = numCasillas;
         numFichas = numF;
-        int[] fichas = new int[numFichas];
+        //int[] fichas = new int[numFichas];
         
     }
 
@@ -19,13 +20,26 @@ public class Tablero{
     Ficha ficha = new Ficha(solicitarFichas());
 
 
+    public void turnos(){
+        int contador = 0;
+        while(contador < fichas.length){ // En esta linea mediante un contador y utilizando 
+            fichas[contador].avanzar(); // el largo con la fila de fichas, hace que cada ficha avance
+            contador += 1;               
+
+        }
+
+    }
+
+
+    
     public void ganador(){
-        for (int i=0; i<fichas.size(); i++ ){
+        int largoLista = fichas.length;
+        for (int i=0; i < largoLista; i++ ){
             if (i.posicion >= casillas){
                 System.out.println("El ganador es:  " );
                 break;
             }
         }
     }
-
+    
 }
